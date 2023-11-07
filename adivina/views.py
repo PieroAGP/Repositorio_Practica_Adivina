45e9be092975deb68a8bb6d78da9d73202a41217
PieroAGP <xpgabpar357@ieshnosmachado.org>
2023-11-07 10:56:12 +0100

    Creación de vistas/templates

--- adivina/views.py
+++ adivina/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+
+def home(request):
+    return render(request, 'adivina/home.html')
+
+def guess(request):
+    random_number=random.randint(1,100)
+    user_guess = int(request.POST['guess'])
+
+    if user_guess == random_number:
+        message='Congratulations'
+    else:
+        message='You failed'
+    
+    return render(request, 'adivina/guess.html',{'message':message})
